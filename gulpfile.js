efc9a486fe737a50cd0bff19e30514231e82a7e2
ArcanisCz <arcaniscz@gmail.com>
2018-03-31 23:53:53 +0200

    rollbar

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+const https = require('https');
+const querystring = require('querystring');
 const gulp = require('gulp');
 const ftp = require('vinyl-ftp');
 const minimist = require('minimist');
@@ -16,9 +18,31 @@ gulp.task('clean', () => {
     return conn.clean("*", ".", {base: '/'});
 });
 
-gulp.task('deploy', ["clean"], () => {
+gulp.task('upload', ["clean"], () => {
     const conn = ftp.create(ftpConfig);
     gulp.src(['./dist/index.html', './dist/*.js', './public/**/*'])
         .pipe(conn.newer(remotePath))
         .pipe(conn.dest(remotePath));
 });
+
+const options = {
+    hostname: 'api.rollbar.com',
+    path: '/api/1/deploy/',
+    method: 'POST',
+};
+const revision = require('child_process')
+    .execSync('git rev-parse HEAD')
+    .toString().trim();
+
+const postData = querystring.stringify({
+    access_token: args.accessToken,
+    environment: "production",
+    revision,
+    local_username: "Circle CI",
+});
+
+gulp.task('deploy', ["upload"], () => new Promise((resolve, reject) => {
+    const req = https.request(options, resolve, reject);
+    req.write(postData);
+    req.end();
+}));

cc5d87f5927526b954ec8e88217a4c4587d14364
ArcanisCz <arcaniscz@gmail.com>
2018-09-25 22:46:32 +0200

    without jss

--- src/Root.js
+++ src/Root.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {ThemeProvider} from 'react-jss';
 
 import App from './app/App';
 
 // eslint-disable-next-line react/prop-types
-export default ({store, theme}) => (
+export default ({store}) => (
     <Provider store={store}>
-        <ThemeProvider theme={theme}>
-            <App />
-        </ThemeProvider>
+        <App />
     </Provider>
 );
--- src/app/App.js
+++ src/app/App.js
@@ -1,32 +1,9 @@
 import React from 'react';
 import PropTypes from "prop-types";
 import {hot} from 'react-hot-loader';
-import injectSheet from 'react-jss';
 import {connect} from "react-redux";
 
 import init from "core/init";
-
-const styles = (theme) => ({
-    "@global": {
-        html: {
-            fontFamily: theme.font.family,
-            margin: 0,
-            padding: 0,
-            width: "100%",
-            height: "100%",
-        },
-        body: {
-            margin: 0,
-            padding: 0,
-            width: "100%",
-            height: "100%",
-        },
-        "#app": {
-            width: "100%",
-            height: "100%",
-        },
-    },
-});
 
 const App = ({loading}) => !loading && <div>game</div>;
 
@@ -39,8 +16,7 @@ const mapStateToProps = (state) => ({
 });
 
 const Connected = connect(mapStateToProps)(App);
-const Styled = injectSheet(styles)(Connected);
-const Hot = hot(module)(Styled);
+const Hot = hot(module)(Connected);
 
 export default Hot;
 

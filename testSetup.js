d6f1cf9f9b1a7b47837392e5125e8d502f1fb7ae
ArcanisCz <arcaniscz@gmail.com>
2018-11-15 20:11:16 +0100

    test fix

--- testSetup.js
+++ testSetup.js
@@ -1,8 +1,6 @@
 import chai from 'chai';
 import chaiSinon from 'sinon-chai';
 import dirtyChai from 'dirty-chai';
-import chaiEnzyme from 'chai-enzyme';
 
-chai.use(chaiEnzyme());
 chai.use(dirtyChai);
 chai.use(chaiSinon);

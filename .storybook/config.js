4d8aa00f27ae9a282b438f3cf1667451a423f86f
ArcanisCz <arcaniscz@gmail.com>
2018-11-18 13:57:35 +0100

    storybook stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,19 +2,30 @@ import React from 'react';
 import {configure, addDecorator, storiesOf} from '@storybook/react';
 import {withKnobs} from '@storybook/addon-knobs';
 
-const req = require.context('../src/components', true, /\.story\.js$/);
-const regexp = /\.\/(.*?)\.story\.js/;
+const FILENAME_REGEXP = /\..*\/(.*?)\.story\.js/;
+const context = require.context('../src/components', true, /\.story\.js$/);
+const stories = storiesOf("Components", module);
+
+addDecorator(withKnobs);
+configure(loadStories, module);
 
 function loadStories() {
-    req.keys().forEach((filename) => {
-        const [Component, props] = req(filename).default;
-        const name = regexp.exec(filename)[1];
-        storiesOf(name, module).add("Default", () => (
-            <Component {...props}/>
-        ));
+    context.keys().forEach((filename) => {
+        const story = context(filename).default;
+        const name = FILENAME_REGEXP.exec(filename)[1];
+        if(Array.isArray(story)){
+            addStory(stories, story, name);
+        } else {
+            Object.keys(story).forEach((subName) => {
+                addStory(stories, story[subName], `${name}/${subName}`);
+            })
+        }
     });
 }
 
-addDecorator(withKnobs);
-
-configure(loadStories, module);
+function addStory(stories, story, name) {
+    const [Component, createProps] = story;
+    stories.add(name, () => (
+        <Component {...createProps()}/>
+    ));
+}
--- src/components/Button.story.js
+++ src/components/Button.story.js
@@ -2,9 +2,9 @@ import {action, boolean, text} from 'story/util';
 
 import {Button} from './Button';
 
-export default [Button, {
+export default [Button, () => ({
     onClick: action('clicked'),
     text: text("Text", "Button"),
     disabled: boolean("Disabled", false),
     block: boolean("Block", false),
-}];
+})];

9af4c79d0384357cc8d58befa533a09264c55e9e
ArcanisCz <arcaniscz@gmail.com>
2018-11-19 02:02:29 +0100

    ci upload

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('clean', () => {
 
 gulp.task('upload', ["clean"], () => {
     const conn = ftp.create(ftpConfig);
-    gulp.src(['./dist/index.html', './dist/*.js', './dist/*.css', './public/*'])
+    gulp.src(['./dist/index.html', './dist/*.js', './dist/*.css', './public/.htaccess'])
         .pipe(conn.newer(remotePath))
         .pipe(conn.dest(remotePath));
     gulp.src(['./dist/storybook/*.html', './dist/storybook/*.ico', './dist/storybook/*.css'])

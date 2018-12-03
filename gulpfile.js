cac4bd8e1f1b534163163a1a5f194658687f1ac0
ArcanisCz <arcaniscz@gmail.com>
2018-12-03 20:34:23 +0100

    temp favicon

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('clean', () => {
 
 gulp.task('upload', ["clean"], () => {
     const conn = ftp.create(ftpConfig);
-    gulp.src(['./dist/index.html', './dist/*.js', './dist/*.css', './public/.htaccess'])
+    gulp.src(['./dist/index.html', './dist/*.js', './dist/*.css', './public/.htaccess', './public/favicon.ico'])
         .pipe(conn.newer(remotePath))
         .pipe(conn.dest(remotePath));
     gulp.src(['./dist/storybook/*.html', './dist/storybook/*.ico', './dist/storybook/*.css'])

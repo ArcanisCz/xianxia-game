8c45763ece138c88b7e2d9c49eb5ce972a8313ad
ArcanisCz <arcaniscz@gmail.com>
2019-02-06 19:57:26 +0100

    redux devtools deprecation update

--- src/createStore.js
+++ src/createStore.js
@@ -9,10 +9,12 @@ export default (saga, initialActions = []) => {
 
     if (saga) {
         sagaMiddleware = createSagaMiddleware();
+        /* eslint-disable no-underscore-dangle */
         middleware = compose(
             applyMiddleware(sagaMiddleware),
-            window.devToolsExtension ? window.devToolsExtension() : (x) => x,
+            window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (x) => x,
         );
+        /* eslint-enable */
     }
 
     const store = createStore(reducer, middleware);

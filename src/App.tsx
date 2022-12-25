4a7f9affd6a8122bf125a6ae2fede6e6fda7e4e1
Tomáš Jílek <arcaniscz@gmail.com>
2022-12-25 16:56:19 +0100

    changing activities

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { withRootStore } from './gameProvider';
 import css from './app.module.css';
 import { ActivityTime } from './core/activity';
+import { ActivityKeys } from './game/activities';
 
 export const App = withRootStore(({ game }) => {
   const pokus = () => {
@@ -11,25 +12,39 @@ export const App = withRootStore(({ game }) => {
     }
   };
 
+  const changeActivity = (time: ActivityTime, activity: ActivityKeys) => {
+    game.changeActivity(time, activity);
+  };
+
   return (
     <div className={css.pokus}>
       <div>Day: {game.activeActivity.day.name}</div>
+      <div>
+        {game.availableActivities[ActivityTime.Day].map(activity => (
+          <button
+            key={activity.id}
+            disabled={activity.id === game.activeActivity.day.id}
+            onClick={() => changeActivity(ActivityTime.Day, activity.id)}
+          >
+            {activity.name}
+          </button>
+        ))}
+      </div>
       <div>Night: {game.activeActivity.night.name}</div>
+      <div>
+        {game.availableActivities[ActivityTime.Night].map(activity => (
+          <button
+            key={activity.id}
+            disabled={activity.id === game.activeActivity.night.id}
+            onClick={() => changeActivity(ActivityTime.Night, activity.id)}
+          >
+            {activity.name}
+          </button>
+        ))}
+      </div>
       <hr />
       <div>Loc: {game.currentLocation.name}</div>
       <hr />
-      <div>
-        Day activities:{' '}
-        {game.availableDayActivities[ActivityTime.Day]
-          .map(a => a.name)
-          .join(', ')}
-      </div>
-      <div>
-        Night activities:{' '}
-        {game.availableDayActivities[ActivityTime.Night]
-          .map(a => a.name)
-          .join(', ')}
-      </div>
       <button onClick={pokus}>aaa</button>
     </div>
   );
--- src/core/game.ts
+++ src/core/game.ts
@@ -1,4 +1,4 @@
-import { observable, makeObservable, action, computed } from 'mobx';
+import { action, computed, makeObservable, observable } from 'mobx';
 import { Activity, ActivityTime } from './activity';
 import { Location } from './location';
 
@@ -8,7 +8,7 @@ export class Game<Activities extends string, Locations extends string> {
     readonly locationRegistry: {
       [key in Locations]: Location<Locations, Activities>;
     },
-    emptyActivity: Activity<Activities>,
+    readonly emptyActivity: Activity<Activities>,
     emptyLocation: Location<Locations, Activities>,
   ) {
     this.activeActivity = {
@@ -29,22 +29,68 @@ export class Game<Activities extends string, Locations extends string> {
   currentLocation: Location<Locations, Activities>;
 
   @computed
-  get availableDayActivities(): {
+  get availableActivities(): {
     [ActivityTime.Day]: Activity<Activities>[];
     [ActivityTime.Night]: Activity<Activities>[];
   } {
+    // TODO: remove duplicates?
+    // TODO: maybe categories?
     return {
       [ActivityTime.Day]: [
+        this.emptyActivity,
         ...this.currentLocation.activities[ActivityTime.Day],
       ],
       [ActivityTime.Night]: [
+        this.emptyActivity,
         ...this.currentLocation.activities[ActivityTime.Night],
       ],
+    };
+  }
+
+  @computed
+  get availableActivitiesSet(): {
+    [ActivityTime.Day]: Set<Activities>;
+    [ActivityTime.Night]: Set<Activities>;
+  } {
+    return {
+      [ActivityTime.Day]: new Set(
+        this.availableActivities[ActivityTime.Day].map(a => a.id),
+      ),
+      [ActivityTime.Night]: new Set(
+        this.availableActivities[ActivityTime.Night].map(a => a.id),
+      ),
     };
   }
 
   @action
   changeLocation(newLocation: Locations) {
     this.currentLocation = this.locationRegistry[newLocation];
+
+    this.checkActiveActivities();
+  }
+
+  @action
+  changeActivity(time: ActivityTime, activity: Activities) {
+    this.activeActivity[time] = this.activityRegistry[activity];
+
+    this.checkActiveActivities();
+  }
+
+  private checkActiveActivities() {
+    if (
+      !this.availableActivitiesSet[ActivityTime.Day].has(
+        this.activeActivity[ActivityTime.Day].id,
+      )
+    ) {
+      this.activeActivity[ActivityTime.Day] = this.emptyActivity;
+    }
+
+    if (
+      !this.availableActivitiesSet[ActivityTime.Night].has(
+        this.activeActivity[ActivityTime.Night].id,
+      )
+    ) {
+      this.activeActivity[ActivityTime.Night] = this.emptyActivity;
+    }
   }
 }
